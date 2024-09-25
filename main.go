4cf946e9c7cf875f6d4129f13786850e8504e8fb
ombima56 <ombimahillary6@gmail.com>
2024-09-25 03:07:22 +0300

    feat: implement prediction of next value using linear regression and calculate range based on standard deviation

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
+
+	"learn.zone01kisumu.ke/git/hiombima/guess-it-2/calculation"
 )
 
 func main() {
@@ -25,5 +28,20 @@ func main() {
 			return
 		}
 		data = append(data, num)
+
+		slope, intercept := calculation.LinearRegression(data)
+		nextX := float64(len(data))
+
+		predictedValue := slope*nextX + intercept
+
+		lowerLimit := predictedValue - 2*calculation.StandardDeviation(data)
+		upperLimit := predictedValue - 2*calculation.StandardDeviation(data)
+
+		fmt.Printf("%.0f %0.f\n", lowerLimit, upperLimit)
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Error reading input: %s", err)
 	}
 }
